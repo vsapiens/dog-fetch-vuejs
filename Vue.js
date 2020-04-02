b9a3c21c5a8b056113fa96dec1880658837dc2f2
vsapiens <erickfrank@icloud.com>
2020-04-01 18:37:17 -0600

    Finished

---
+++ Vue.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "",
+    seen: false,
+    username: "",
+    counter1: 0,
+    counter2: 0,
+    myImage: ""
+  },
+  methods: {
+    changeUserEnter: function() {
+      this.message = "Welcome " + this.username;
+      this.username = "";
+      this.counter1++;
+    },
+    changeUserClick: function() {
+      this.message = "Welcome " + this.username;
+      this.username = "";
+      this.counter2++;
+    },
+    changeSeen: function() {
+      this.seen = !this.seen;
+    },
+    fetchDog: function() {
+      let url = "https://dog.ceo/api/breeds/image/random";
+      let settings = {
+        method: "GET"
+      };
+
+      fetch(url, settings)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJSON => {
+          this.myImage = responseJSON.message;
+        });
+    }
+  }
+});
